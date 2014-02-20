10d7cb10a89052e90bdbcdd479405d8f1dca5cca
Albin Larsson <albin.post@gmail.com>
2014-02-20 16:28:29 +0100

    from tabs to spaces

--- js/map_config.js
+++ js/map_config.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
-	zoomControl: false
+  zoomControl: false
 });
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: 'Map data &copy; <a href="http://openstr">OpenStreetMap</a> contributors.'
+  attribution: 'Map data &copy; <a href="http://openstr">OpenStreetMap</a> contributors.'
 }).addTo(map);
